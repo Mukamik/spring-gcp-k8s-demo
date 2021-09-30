fe93ca6b35190c9bd4c8e660088b3d05349a39b2
aaronmwanjala <aaronmwanjala@google.com>
2021-09-30 10:28:52 -0400

    502 from xe image, shelving Oracle portion for now

--- src/test/java/com/wanjala/gcpdemo/GcpDemoApplicationTests.java
+++ src/test/java/com/wanjala/gcpdemo/GcpDemoApplicationTests.java
@@ -2,14 +2,16 @@ package com.wanjala.gcpdemo;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.MySQLContainer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -17,46 +19,67 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @Testcontainers
 class GcpDemoApplicationTests {
 
-	private static final Logger logger = LoggerFactory.getLogger(GcpDemoApplicationTests.class);
+    private static final Logger logger = LoggerFactory.getLogger(GcpDemoApplicationTests.class);
 
-	public static final DockerImageName MYSQL_57_IMAGE = DockerImageName.parse("mysql:5.7.34");
+    @ClassRule
+    public static OracleContainer oracleContainer = new OracleContainer("phx.ocir.io/toddrsharp/oracle-db/oracle/database:18.4.0-xe")
+            .withLogConsumer(new Slf4jLogConsumer(logger));
 
-	@Test
-	void testSimple() throws SQLException {
-		try(MySQLContainer<?> mysql = new MySQLContainer<>(MYSQL_57_IMAGE)
-				.withDatabaseName("test")
-				.withLogConsumer(new Slf4jLogConsumer(logger))
-		) {
-			mysql.start();
+    @BeforeAll
+    public static void startup() {
+        oracleContainer.start();
+    }
 
-			ResultSet resultSet = performQuery(mysql, "SELECT 1");
-			int resultSetInt = resultSet.getInt(1);
+    @Test
+    void testSimple() throws SQLException {
+        try {
+            oracleContainer.start();
 
-			assertEquals("Basic select query works", 1, resultSetInt);
-		}
-	}
 
-	protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
-		DataSource ds = getDataSource(container);
-		Statement statement = ds.getConnection().createStatement();
-		statement.execute(sql);
-		ResultSet resultSet = statement.getResultSet();
+            ResultSet resultSet = performQuery("SELECT 1");
+            int resultSetInt = resultSet.getInt(1);
 
-		resultSet.next();
-		return resultSet;
-	}
+            assertEquals("Basic select query works", 1, resultSetInt);
+        } catch (SQLException e) {
+            fail("Unexpected error" + e.getStackTrace());
+        }
+    }
 
-	protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
-		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setJdbcUrl(container.getJdbcUrl());
-		hikariConfig.setUsername(container.getUsername());
-		hikariConfig.setPassword(container.getPassword());
-		hikariConfig.setDriverClassName(container.getDriverClassName());
-		return new HikariDataSource(hikariConfig);
-	}
+    @TestConfiguration
+    static class OracleTestConfiguration {
+
+        @Bean
+        DataSource dataSource() {
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setJdbcUrl(oracleContainer.getJdbcUrl());
+            hikariConfig.setUsername(oracleContainer.getUsername());
+            hikariConfig.setPassword(oracleContainer.getPassword());
+            return new HikariDataSource(hikariConfig);
+        }
+    }
+
+    protected ResultSet performQuery(String sql) throws SQLException {
+        DataSource ds = getDataSource();
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute(sql);
+        ResultSet resultSet = statement.getResultSet();
+
+        resultSet.next();
+        return resultSet;
+    }
+
+    protected DataSource getDataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(oracleContainer.getJdbcUrl());
+        hikariConfig.setUsername(oracleContainer.getUsername());
+        hikariConfig.setPassword(oracleContainer.getPassword());
+        hikariConfig.setDriverClassName(oracleContainer.getDriverClassName());
+        return new HikariDataSource(hikariConfig);
+    }
 
 }
