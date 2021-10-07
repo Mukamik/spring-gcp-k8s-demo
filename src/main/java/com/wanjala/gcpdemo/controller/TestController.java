a78584cd094452f1e9b4d53fc7ec9444ff110ac9
aaronmwanjala <aaronmwanjala@google.com>
2021-10-07 17:18:40 -0400

    REFACTOR: to 11

--- src/main/java/com/wanjala/gcpdemo/controller/TestController.java
+++ src/main/java/com/wanjala/gcpdemo/controller/TestController.java
@@ -1,21 +1,19 @@
 package com.wanjala.gcpdemo.controller;
 
-import com.wanjala.gcpdemo.models.ExampleRecord;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.UUID;
-
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
-  @GetMapping("/okay")
-  public ResponseEntity<ExampleRecord> okay() {
-    System.out.println("Replying with test record:");
-    return new ResponseEntity<>(new ExampleRecord(1, "Test record", UUID.randomUUID()), HttpStatus.OK);
+  @GetMapping
+  public ResponseEntity<String> okay() {
+    System.out.println("Request received at /test");
+    return new ResponseEntity<>("Request received - " + UUID.randomUUID(), HttpStatus.OK);
   }
 }
--- src/main/java/com/wanjala/gcpdemo/models/ExampleRecord.java
+++
@@ -1,6 +0,0 @@
-package com.wanjala.gcpdemo.models;
-
-import java.util.UUID;
-
-public record ExampleRecord(int Id, String name, UUID parentId) {
-}
