ef5b14aa7d6e257104c9194916d541d77bafc5b7
aaronmwanjala <aaronmwanjala@google.com>
2021-10-11 06:55:50 -0400

    REFACTOR: 11, paired down again, added layers config

--- src/main/java/com/wanjala/gcpdemo/controller/TestController.java
+++ src/main/java/com/wanjala/gcpdemo/controller/TestController.java
@@ -1,20 +1,15 @@
 package com.wanjala.gcpdemo.controller;
 
-import com.wanjala.gcpdemo.models.DemoRecord;
-import java.util.UUID;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/test")
+@RestController
+@RequestMapping("/")
 public class TestController {
 
-  @GetMapping("/okay")
-  public ResponseEntity<DemoRecord> okay() {
-    System.out.println("Replying with test record:");
-    return new ResponseEntity<>(new DemoRecord(1, "Test record", UUID.randomUUID()), HttpStatus.OK);
+  @GetMapping
+  public String okay() {
+    return "Hello NY JavaSIG!";
   }
 }
--- src/main/java/com/wanjala/gcpdemo/models/DemoRecord.java
+++
@@ -1,6 +0,0 @@
-package com.wanjala.gcpdemo.models;
-
-import java.util.UUID;
-
-public record DemoRecord(int Id, String name, UUID parentId) {
-    }
