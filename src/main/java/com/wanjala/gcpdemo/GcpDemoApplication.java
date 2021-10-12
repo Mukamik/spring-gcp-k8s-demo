31e863f6477a81c281fb764d7933b80c4a661dd6
aaronmwanjala <aaronmwanjala@google.com>
2021-10-11 21:28:23 -0400

    Minimum config?

--- src/main/java/com/wanjala/gcpdemo/GcpDemoApplication.java
+++ src/main/java/com/wanjala/gcpdemo/GcpDemoApplication.java
@@ -1,15 +1,36 @@
 package com.wanjala.gcpdemo;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories("com.wanjala.gcpdemo.repository")
 public class GcpDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcpDemoApplication.class, args);
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+		final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+
+		final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		return em;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		return dataSource;
 	}
 
 }
