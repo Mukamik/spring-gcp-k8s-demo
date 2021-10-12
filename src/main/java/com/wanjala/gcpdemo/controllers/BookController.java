8123d6c5f22add75052f17450e0d73058919b6fd
aaronmwanjala <aaronmwanjala@google.com>
2021-10-12 14:34:27 -0400

    REFACTOR: controller

--- src/main/java/com/wanjala/gcpdemo/controllers/BookController.java
+++ src/main/java/com/wanjala/gcpdemo/controllers/BookController.java
@@ -4,11 +4,11 @@ import com.wanjala.gcpdemo.models.Book;
 import com.wanjala.gcpdemo.services.BookService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/books")
 public class BookController {
 
